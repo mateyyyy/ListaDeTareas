742f0553682d9266862ae93d6c3cfadbd6151a2e
mateyyyy <65136286+mateyyyy@users.noreply.github.com>
2024-08-31 12:28:47 -0300

    ManosAlaObra2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { MdDelete } from "react-icons/md";
+import { useState } from 'react'
 import './App.css'
+import VentanaPrincipal from './components/molecules/VentanaPrincipal'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div id='principalContainer'>
-        <h1>LISTA DE TAREAS</h1>
-        <form action="">
-          <input type="text" />
-          <button>ADD</button>
-        </form>
-        <div id='tareasContainer'></div>
-          <div className='tareaDiv'>
-            <input type="checkbox" />
-            <h2>Cocinar</h2>
-            <button><MdDelete /></button>
-          </div>
-
-          <div className='tareaDiv'>
-            <input type="checkbox" />
-            <h2>Estudiar</h2>
-            <button><MdDelete /></button>
-          </div>
-
-          <div className='tareaDiv'>
-            <input type="checkbox" checked/>
-            <h2 id="jugar">Jugar</h2>
-            <button><MdDelete /></button>
-          </div>
-
-      </div>
+      <VentanaPrincipal></VentanaPrincipal>
     </>
   )
 }
---
+++ src/components/atoms/Input/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {inputEntrada} from './Input.module.css'
+
+export default function Input() {
+  return (
+    <input type="text" id={inputEntrada}/>
+  )
+}
