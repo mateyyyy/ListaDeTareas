648ef9ae24dabd53e1afc8eb78d3b5dddbddce39
Mateyyyy <matiixd11@gmail.com>
2024-09-04 16:49:38 -0300

    commit2

--- src/components/molecules/Tarea/index.jsx
+++ src/components/molecules/Tarea/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TrashCanButon from '../../atoms/TrashCanButon'
 import styles from './Tarea.module.css'
 
 export default function Tarea({nombre, deleteTask}) {
 
+  const [checked, setCheck] = useState(false);
   const activarDelete = () => {
     deleteTask(nombre)
   }
   return (
     <div id={styles.contenedorTarea}>
-        <input type="checkbox" />
-        <h2>{nombre}</h2>
+        <input type="checkbox" onChange={() => setCheck(!checked)}/>
+        <h2 className={checked? styles.check : styles.notCheck}>{nombre}</h2>
         <TrashCanButon deleteTask={activarDelete}></TrashCanButon>
     </div>
   )
